cecc89cc98bea371fefd9ffaf241da64ceb3c0d4
Soham <sprobust@gmail.com>
2025-07-19 08:22:32 +0000

    YES NOW MAKE IT 3/5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -354,7 +354,7 @@ export default function Home() {
               </div>
             )}
             {sessions.map(session => (
-              <div key={session.id} className="group relative w-1/2">
+              <div key={session.id} className="group relative w-3/5">
                 <Button
                   size="sm"
                   variant={session.id === activeChatId ? "default" : "outline"}
