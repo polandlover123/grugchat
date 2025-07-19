f2221ccca6e6d0fc65f2790a927fea0d357e1eb0
Soham <sprobust@gmail.com>
2025-07-19 08:18:52 +0000

    do 9/10 for new chat
    i've noticed that the dynamic ones dont update at

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { useState, useRef, useEffect, type SVGProps } from "react";
@@ -338,7 +339,7 @@ export default function Home() {
           </div>
         </div>
         <div className="p-4">
-          <Button size="sm" variant="outline" className="w-4/5 justify-start" onClick={() => fileInputRef.current?.click()}>
+          <Button size="sm" variant="outline" className="w-9/10 justify-start" onClick={() => fileInputRef.current?.click()}>
             <Plus className="mr-2 h-4 w-4 flex-shrink-0" />
             <span className="truncate">New Chat</span>
           </Button>
@@ -358,7 +359,7 @@ export default function Home() {
                   size="sm"
                   variant={session.id === activeChatId ? "default" : "outline"}
                   onClick={() => selectChat(session.id)}
-                  className="w-1/20 justify-start pr-8"
+                  className="w-9/10 justify-start pr-8"
                 >
                   <MessageSquare className="mr-2 h-4 w-4 flex-shrink-0" />
                   <span className="truncate">{session.pdfFile.name}</span>
@@ -403,3 +404,5 @@ export default function Home() {
     </div>
   );
 }
+
+    
